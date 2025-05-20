2b400da213b899e15590ed5d73ba9cc9c3c180d5
niraj-goldengate <niraj.goldengate@gmail.com>
2025-05-20 14:41:23 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+// server/server.js
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const invitationRoutes = require('./routes/invitations');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+const attendanceRoutes = require('./routes/attendance');  
+const adminRoutes = require('./routes/admin');
+
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+
+// Connect to MongoDB Atlas
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+app.use('/api/invitations', invitationRoutes);
+
+app.use('/api/users', userRoutes);
+
+app.use('/api/attendance', attendanceRoutes);
+
+app.use('/api/admin', adminRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
+
