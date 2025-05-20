04308b4eacfb5aceb5580326326d73eef9a7c84f
niraj-goldengate <niraj.goldengate@gmail.com>
2025-05-20 17:14:52 +0530

    Delete api directory

--- api/index.js
+++
@@ -1,8 +0,0 @@
-// api/index.js
-const app = require('../server/app');
-
-// Vercel (with `@vercel/node`) will detect and call this:
-module.exports = (req, res) => {
-  // Forward the request into your Express app
-  app(req, res);
-};
