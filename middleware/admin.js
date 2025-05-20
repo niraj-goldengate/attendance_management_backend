e6a022a50727b1873ed58948472827e0bb00926b
niraj-goldengate <niraj.goldengate@gmail.com>
2025-05-20 14:46:49 +0530

    Add files via upload

---
+++ middleware/admin.js
@@ -0,0 +1,9 @@
+// server/middleware/admin.js
+module.exports = function (req, res, next) {
+    if (req.user && req.user.role === 'admin') {
+      next();
+    } else {
+      res.status(403).json({ message: 'Access denied. Admins only.' });
+    }
+  };
+  
